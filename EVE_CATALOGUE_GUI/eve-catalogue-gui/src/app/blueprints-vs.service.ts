24f8bd2af8c5a0e56ff8139350cf999e69e5203d
Francesca Moscatelli <f.moscatelli@nextworks.it>
2019-10-08 18:06:20 +0200

    GET VS Blueprints + VS Blueprints wizard logic initial commit

--- EVE_CATALOGUE_GUI/eve-catalogue-gui/src/app/blueprints-vs.service.ts
+++ EVE_CATALOGUE_GUI/eve-catalogue-gui/src/app/blueprints-vs.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
-import { BlueprintsVsDataSource, BlueprintsVsItem } from './blueprints-vs/blueprints-vs-datasource';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { VsBlueprintInfo } from './blueprints-vs/vs-blueprint-info';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlueprintsVsService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8082/portal/catalogue/';
+  private vsBlueprintInfoUrl = 'vsblueprint';
 
-  getVsBlueprints(): BlueprintsVsDataSource {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-    return new BlueprintsVsDataSource();
+  constructor(private http: HttpClient) { }
+
+  /*getVsBlueprints(): Observable<VsBlueprintInfo[]> {
+    return this.http.get<VsBlueprintInfo[]>(this.baseUrl + this.vsBlueprintInfoUrl);
+  }*/
+
+  getVsBlueprints(): Observable<VsBlueprintInfo[]> {
+    return this.http.get<VsBlueprintInfo[]>(this.baseUrl + this.vsBlueprintInfoUrl);
+  }
+
+  postVsBlueprint(data) {
+    this.http.post(this.baseUrl + this.vsBlueprintInfoUrl, data);
   }
 }
