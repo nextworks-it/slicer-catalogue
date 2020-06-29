4a31be41adbbbcda3faec9cc1565195da7ee1769
j.brenes <j.brenes@nextworks.it>
2020-06-29 12:04:53 +0200

    fix: added missing method

--- VS_BLUEPRINTS_CATALOGUE/src/main/java/it/nextworks/nfvmano/catalogue/blueprint/services/AuthService.java
+++ VS_BLUEPRINTS_CATALOGUE/src/main/java/it/nextworks/nfvmano/catalogue/blueprint/services/AuthService.java
@@ -2,15 +2,21 @@ package it.nextworks.nfvmano.catalogue.blueprint.services;
 
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Set;
 
 @Service
 public class AuthService {
 
-
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
     @Value("${authentication.enable}")
     private boolean authenticationEnable;
 
@@ -40,6 +46,28 @@ public class AuthService {
         }else return adminTenant;
 
 
+    }
+
+    public Set<String> getUserRoles(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(authenticationEnable){
+            if(keycloakEnabled){
+                if (auth.getPrincipal() instanceof KeycloakPrincipal) {
+                    KeycloakPrincipal<KeycloakSecurityContext> kp = (KeycloakPrincipal<KeycloakSecurityContext>) auth.getPrincipal();
+                    Set<String> roles = kp.getKeycloakSecurityContext().getToken().getRealmAccess().getRoles();
+                    log.debug("Retrieved user roles: "+roles);
+                    return roles;
+                }else return Collections.emptySet();
+            }else{
+                return Collections.emptySet() ;
+
+            }
+
+        }else return Collections.emptySet();
+    }
+
+    public String getUser(){
+        return getUserFromAuth(SecurityContextHolder.getContext().getAuthentication());
     }
 
 
